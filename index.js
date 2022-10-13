6cf305633f2b7ea17658cc078830504c3fe3990f
Miguel Rodriguez <mrodriguez@gmail.com>
2022-10-13 18:25:31 -0500

    Sesion 2 L58

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
 const express = require('express');
 const Joi = require('joi');
 const app = express();
+const logger = require('./logger')
+const sucursales = require('./routes/sucursales')
 
 app.use(express.json());
+app.use(logger)
 
-const sucursales  = [
-    { id: 1, nombre: 'Mexico'},
-    { id: 2, nombre: 'Gustavo Baz'},
-    { id: 3, nombre: 'Guadalajara'},
-]
+app.use('/sucursales', sucursales)
 
 app.get('/', (req, res) => {
     res.status(200).send('Hola Mundo');
-});
-
-app.get('/sucursales/', (req, res) => {
-    res.status(200).send(sucursales);
-});
-
-app.get('/sucursales/:id', (req, res) => {
-    const sucursal = sucursales.find(i => i.id === parseInt(req.params.id))
-    if (!sucursal) res.status(400).send('Esa sucursal no existe')
-    res.status(200).send(sucursal);
-});
-
-app.post('/sucursales', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required(),
-        x: Joi.number(),
-    })
-
-    const r = schema.validate(req.body)
-    if (r.error) {
-        res.status(400).send(r.error.message)
-        return
-    }
-
-    const {name, x} = req.body
-    const sucursal = {
-        id: sucursales.length + 1,
-        name: name,
-        x: x
-    }
-    sucursales.push(sucursal);
-    res.send(sucursales);
 });
 
 const port = process.env.PORT || 3000;
